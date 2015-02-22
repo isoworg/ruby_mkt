07dffdf3de7187f93f07905c6ede4086d79bc770
Will Bentinck <will@bentinck.net>
2015-02-22 16:23:02 +0000

    tests begun

--- spec/marketplace_spec.rb
+++ spec/marketplace_spec.rb
@@ -1,3 +1,20 @@
-Rspec.describe "the marketplace" do
-it ""
+RSpec.describe 'the marketplace' do
+  it 'has products' do
+    Product.new
+  end
+
+  it 'has promotional rules'
+  it 'can change the promotional rules'
+  it 'scans items in any order'
+
+  context 'when the checkout total is greater than £60' do
+    it 'gives a 10% discount'
+  end
+
+  context 'when the basket includes 2 or more lavender hearts' do
+    it 'reduces the price of a lavender heart to £8.50'
+  end
+
+
+
 end
